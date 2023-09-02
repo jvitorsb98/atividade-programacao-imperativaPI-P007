f0de0846b32a979be3a1f40696ab1bff238d420e
João Ramos <jvitorsb98@outlook.com>
2023-09-02 16:15:15 -0300

    merge realizado

--- exercicio11.cpp
+++ exercicio11.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*Escreva um programa em C++ que leia um número inteiro e imprima a sequência de Fibonacci (Referência abaixo) até o número fornecido pelo usuário.*/
 
 #include <iostream>
@@ -28,3 +29,41 @@ int main()
     }
     cout << endl;
 }
+=======
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int numero;
+    int numDigitos = 0;
+    int temp;
+    int soma = 0;
+    cout << "Entre com um numero: ";
+    cin >> numero;
+
+    temp = numero;
+
+    while (temp > 0) {
+        temp /= 10;
+        numDigitos++;
+    }
+
+    temp = numero;
+
+    while (temp > 0) {
+        int digito = temp % 10;
+        soma += pow(digito, numDigitos);
+        temp /= 10;
+    }
+
+    if (soma == numero) {
+        cout << numero << " é um número Armstrong." << endl;
+    } else {
+        cout << numero << " não é um número Armstrong." << endl;
+    }
+
+    return 0;
+}
+>>>>>>> abb4680 (exercicio de fibonacci inserido)
